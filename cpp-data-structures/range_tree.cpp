9d5d8db55eb8f5dea3430769d281387962bdca97
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-03-01 13:08:24 +0800

    fixing range tree code

--- cpp-data-structures/range_tree.cpp
+++ cpp-data-structures/range_tree.cpp
@@ -13,41 +13,43 @@ using std::vector;
   1. range updates
   2. point queries
  */
-template <typename T>
-class range_tree {
-public:
-  range_tree<T>(size_t n) : n_{n}, tree_{vector<T>(1<<18, 0)} {}
+ template <typename T>
+ class range_tree {
+ public:
+ range_tree<T>(size_t n) : range_tree<T>(n, 4 * n) {}
 
-  void update(size_t uL, size_t uR, T v) {
-    _update(uL, uR, v, 0, n_);
-  }
+ void update(size_t uL, size_t uR, T v) {
+     _update(uL, uR, v, 1, 0, n_);
+ }
 
-  T query(size_t p) {
-    return _query(p, 1, 0, n_);
-  }
+ T query(size_t p) {
+     return _query(p, 1, 0, n_);
+ }
 
-private:
-  void _update(size_t uL, size_t uR, T v, size_t i, size_t cL, size_t cR) {
-    if (uL == cL && uR == cR) {
-      tree_.at(i) += v;
-      return;
-    }
-    size_t mid = (cL + cR) / 2;
-    if (uL < mid) _update(uL, min(uR, mid), v, i * 2, cL, mid);
-    if (uR > mid) _update(max(uL, mid), uR, v, i * 2 + 1, mid, cR);
-  }
+ private:
+ range_tree<T>(size_t n, size_t tree_size) : n_{n}, tree_{vector<T>(tree_size, 0)} {}
 
-  T _query(size_t p, size_t i, size_t cL, size_t cR) {
-    if (cR - cL == 1) {
-      return tree_.at(i);
-    }
-    int mid = (cL + cR) / 2;
-    long long ans = tree_.at(i);
-    if (p < mid) ans += _query(p, i * 2, cL, mid);
-    else ans += _query(p, i * 2 + 1, mid, cR);
-    return ans;
-  }
+ void _update(size_t uL, size_t uR, T v, size_t i, size_t cL, size_t cR) {
+     if (uL == cL && uR == cR) {
+     tree_.at(i) += v;
+     return;
+     }
+     size_t mid = (cL + cR) / 2;
+     if (uL < mid) _update(uL, min(uR, mid), v, i * 2, cL, mid);
+     if (uR > mid) _update(max(uL, mid), uR, v, i * 2 + 1, mid, cR);
+ }
 
-  vector<T> tree_;
-  size_t n_;
-};
+ T _query(size_t p, size_t i, size_t cL, size_t cR) {
+     if (cR - cL == 1) {
+     return tree_.at(i);
+     }
+     int mid = (cL + cR) / 2;
+     long long ans = tree_.at(i);
+     if (p < mid) ans += _query(p, i * 2, cL, mid);
+     else ans += _query(p, i * 2 + 1, mid, cR);
+     return ans;
+ }
+
+ vector<T> tree_;
+ size_t n_;
+ };
