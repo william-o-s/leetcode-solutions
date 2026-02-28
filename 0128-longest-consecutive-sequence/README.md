2ab12848f10741bad10ef44c3bf820cddecc1808
z5267282 <80458779+z5267282@users.noreply.github.com>
2026-02-28 15:41:00 +1100

    final answer: need to remember which starting values you saw

--- 0128-longest-consecutive-sequence/README.md
+++ 0128-longest-consecutive-sequence/README.md
@@ -11,4 +11,50 @@ the element without a right; 4, 5, 6 - there is no 7.
 
 So long as you only visit each streak once by finding its start or end, the
 algorithm should be linear. But that's not the case right now. The Neetcode
-solution runs by finding the start and it's much faster.
+solution runs by finding the start and it's much faster. At the moment, I
+tested each on an input with 50,000 elements and the Python is much faster
+
+```
+> time python3 sol.py
+python3 sol.py  0.02s user 0.01s system 74% cpu 0.043 total
+> time ./sol
+./sol  0.03s user 0.00s system 93% cpu 0.032 total
+```
+
+## The `continue`
+
+It might have been the `continue`.
+
+```
+> time ./sol
+./sol  0.04s user 0.00s system 10% cpu 0.366 total
+```
+
+Originally it was:
+
+```cpp
+for (int n : nums) {
+    if (!seen.contains(n + 1)) {
+```
+
+, but now it is
+
+```cpp
+for (int n : nums) {
+    // n is not the end of a streak
+    if (seen.contains(n + 1)) {
+        continue;
+    }
+    // ...
+```
+
+## The Final Problem
+
+There are test cases where there are many duplicated ends
+
+```
+1, 2, 3, ... 500, 500, 500
+```
+
+I think the Neetcode solution got lucky because there aren't duplicated starts.
+This is easy enough to fix; just remember which ending points you saw before.
--- 0128-longest-consecutive-sequence/sol.cpp
+++ 0128-longest-consecutive-sequence/sol.cpp
@@ -5,37 +5,19 @@
 using namespace std;
 
 int longestConsecutive(vector<int>& nums) {
-    // [100, 4, 200, 1, 3, 2]
-    // the end of a streak won't have e + 1
-
-    // cout << nums.size() << '\n';
-
-    auto seen = unordered_set<int>(nums.begin(), nums.end());
+    auto seen = unordered_set<int>(nums.begin(), nums.end()), ends = unordered_set<int>{};
     int answer = 0;
     for (int n : nums) {
         // n is not the end of a streak
-        if (seen.contains(n + 1)) {
-            // cout << "SKIPPED: " << n << '\n';
-            continue;
+        if (!seen.contains(n + 1) && !ends.contains(n)) {
+            ends.insert(n);
+            int length = 1;
+            while (seen.contains(--n)) {
+                ++length;
+            }
+            answer = max(answer, length);
         }
-        // cout << "START: " << n << '\n';
-        int length = 1;
-        while (seen.contains(--n)) {
-            // cout << "  " << n << '\n';
-            ++length;
-        }
-        answer = max(answer, length);
     }
-
-    return answer;
-}
-
-int main() {
-    // too slow for ints 1 ... 50,000
-    auto big = vector<int>(50 * 1000);
-    for (int i = 1; i <= 50 * 1000; ++i) {
-        big[i - 1] = i;
-    }
-
-    longestConsecutive(big);
+    
+    return answer; 
 }
